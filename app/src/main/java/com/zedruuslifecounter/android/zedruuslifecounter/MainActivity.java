1d8dc7b65c1384d7f2f113c6b0b0d0925f7ddf1c
Nelson Wells <nelsonandrewwells@gmail.com>
2017-03-07 11:40:32 -0800

    Add energy and poison logos. Sketch out methods and initial layout.

--- app/src/main/java/com/zedruuslifecounter/android/zedruuslifecounter/MainActivity.java
+++ app/src/main/java/com/zedruuslifecounter/android/zedruuslifecounter/MainActivity.java
@@ -11,6 +11,10 @@ public class MainActivity extends AppCompatActivity {
 
     int lifePlayerOne = 20;
     int lifePlayerTwo = 20;
+    int poisonPlayerOne = 0;
+    int poisonPlayerTwo = 0;
+    int energyPlayerOne = 0;
+    int energyPlayerTwo = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,16 +22,68 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    private void resetLife(View view){
-        lifePlayerOne = 20;
-        lifePlayerTwo = 20;
+    public void incrementPlayerOneLifeTotal(View view){
+        lifePlayerOne += 1;
     }
 
-    private void revealPoisonButton(View view){
+    public void decrementPlayerOneLifeTotal(View view){
+        lifePlayerOne -= 1;
+    }
+
+    public void incrementPlayerTwoLifeTotal(View view){
+        lifePlayerTwo += 1;
+    }
+
+    public void decrementPlayerTwoLifeTotal(View view){
+        lifePlayerTwo -= 1;
+    }
+
+    public void incrementPlayerOnePoisonCounter(View view){
+        poisonPlayerOne += 1;
+    }
+
+    public void decrementPlayerOnePoisonCounter(View view){
+        poisonPlayerOne -= 1;
+    }
+
+    public void incrementPlayerTwoPoisonCounter(View view){
+        poisonPlayerTwo += 1;
+    }
+
+    public void decrementPlayerTwoPoisonCounter(View view){
+        poisonPlayerTwo -= 1;
+    }
+
+    public void incrementPlayerOneEnergyCounter(View view){
+        energyPlayerOne += 1;
+    }
+
+    public void decrementPlayerOneEnergyCounter(View view){
+        energyPlayerOne -= 1;
+    }
+
+    public void incrementPlayerTwoEnergyCounter(View view){
+        energyPlayerTwo += 1;
+    }
+
+    public void decrementPlayerTwoEnergyCounter(View view){
+        energyPlayerTwo -= 1;
+    }
+
+    public void resetLife(View view){
+        lifePlayerOne = 20;
+        lifePlayerTwo = 20;
+        poisonPlayerOne = 0;
+        poisonPlayerTwo = 0;
+        energyPlayerOne = 0;
+        energyPlayerTwo = 0;
+    }
+
+    public void revealPoisonButton(View view){
         //if (Visibility.GONE == id.poison_button)
     }
 
-    private void revealEnergyButton(View view){
+    public void revealEnergyButton(View view){
         //if (Visibiltiy.GONE == id.energy_button)
     }
 }
