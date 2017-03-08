f6369927c8925776085cd083b4b8e66b000038ed
Nelson Wells <nelsonandrewwells@gmail.com>
2017-03-07 21:08:29 -0800

    Add comments. Add Color Change Button and possible method for v2.0. :) Straighten out some dimens.

--- app/src/main/java/com/zedruuslifecounter/android/zedruuslifecounter/MainActivity.java
+++ app/src/main/java/com/zedruuslifecounter/android/zedruuslifecounter/MainActivity.java
@@ -20,69 +20,117 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void incrementPlayerOneLifeTotal(View view){
+    /**
+     * Add one to Player One's Life Total then display the new total
+     */
+    public void incrementPlayerOneLifeTotal(View view) {
 //        if (view.setOnLongClickListener() == )
         lifePlayerOne += 1;
         displayPlayerOneLifeTotal(lifePlayerOne);
     }
 
-    public void decrementPlayerOneLifeTotal(View view){
+    /**
+     * Subtract one from Player One's Life Total then display the new total
+     * In Magic: the Gathering it is possible for a player to have
+     * a negative life total, so there is no check here.
+     */
+    public void decrementPlayerOneLifeTotal(View view) {
         lifePlayerOne -= 1;
         displayPlayerOneLifeTotal(lifePlayerOne);
     }
 
-    public void incrementPlayerTwoLifeTotal(View view){
+    /**
+     * Add one to Player Two's Life Total then display the new total
+     */
+    public void incrementPlayerTwoLifeTotal(View view) {
         lifePlayerTwo += 1;
         displayPlayerTwoLifeTotal(lifePlayerTwo);
     }
 
-    public void decrementPlayerTwoLifeTotal(View view){
+    /**
+     * Subtract one from Player Two's Life Total then display the new total
+     * In Magic: the Gathering it is possible for a player to have
+     * a negative life total, so there is no check here.
+     */
+    public void decrementPlayerTwoLifeTotal(View view) {
         lifePlayerTwo -= 1;
         displayPlayerTwoLifeTotal(lifePlayerTwo);
     }
 
-    public void incrementPlayerOnePoisonCounter(View view){
+    public void incrementPlayerOnePoisonCounter(View view) {
         poisonPlayerOne += 1;
         displayPlayerOnePoisonTotal(poisonPlayerOne);
     }
 
-    public void decrementPlayerOnePoisonCounter(View view){
+    /**
+     * Used to subtract from a players Poison count total.
+     * Players can not have less than 0 counters.
+     */
+    public void decrementPlayerOnePoisonCounter(View view) {
         poisonPlayerOne -= 1;
+        if (poisonPlayerOne == -1) {
+            poisonPlayerOne = 0;
+        }
         displayPlayerOnePoisonTotal(poisonPlayerOne);
     }
 
-    public void incrementPlayerTwoPoisonCounter(View view){
+    public void incrementPlayerTwoPoisonCounter(View view) {
         poisonPlayerTwo += 1;
         displayPlayerTwoPoisonTotal(poisonPlayerTwo);
     }
 
-    public void decrementPlayerTwoPoisonCounter(View view){
+    /**
+     * Used to subtract from a players Poison count total.
+     * Players can not have less than 0 counters.
+     */
+    public void decrementPlayerTwoPoisonCounter(View view) {
         poisonPlayerTwo -= 1;
+        if (poisonPlayerTwo == -1) {
+            poisonPlayerTwo = 0;
+        }
         displayPlayerTwoPoisonTotal(poisonPlayerTwo);
     }
 
-    public void incrementPlayerOneEnergyCounter(View view){
+    public void incrementPlayerOneEnergyCounter(View view) {
         energyPlayerOne += 1;
         displayPlayerOneEnergyTotal(energyPlayerOne);
     }
 
-    public void decrementPlayerOneEnergyCounter(View view){
+    /**
+     * Used to subtract from a players Energy count total.
+     * Players can not have less than 0 counters.
+     */
+    public void decrementPlayerOneEnergyCounter(View view) {
         energyPlayerOne -= 1;
+        if (energyPlayerOne == -1) {
+            energyPlayerOne = 0;
+        }
         displayPlayerOneEnergyTotal(energyPlayerOne);
     }
 
-    public void incrementPlayerTwoEnergyCounter(View view){
+    public void incrementPlayerTwoEnergyCounter(View view) {
         energyPlayerTwo += 1;
         displayPlayerTwoEnergyTotal(energyPlayerTwo);
 
     }
 
-    public void decrementPlayerTwoEnergyCounter(View view){
+    /**
+     * Used to subtract from a players Energy count total.
+     * Players can not have less than 0 counters.
+     */
+    public void decrementPlayerTwoEnergyCounter(View view) {
         energyPlayerTwo -= 1;
+        if (energyPlayerTwo == -1) {
+            energyPlayerTwo = 0;
+        }
         displayPlayerTwoEnergyTotal(energyPlayerTwo);
     }
 
-    public void resetLife(View view){
+    /**
+     * Reset all values to a new game start then display the new total
+     * A classic Magic: the Gathering game starts players at 20 life.
+     */
+    public void resetLife(View view) {
         lifePlayerOne = 20;
         lifePlayerTwo = 20;
         poisonPlayerOne = 0;
@@ -97,32 +145,32 @@ public class MainActivity extends AppCompatActivity {
         displayPlayerTwoEnergyTotal(energyPlayerTwo);
     }
 
-    public void displayPlayerOneLifeTotal(int lifeTotal){
+    public void displayPlayerOneLifeTotal(int lifeTotal) {
         TextView lifeView = (TextView) findViewById(R.id.life_player_one_text_view);
         lifeView.setText(String.valueOf(lifeTotal));
     }
 
-    public void displayPlayerTwoLifeTotal(int lifeTotal2){
+    public void displayPlayerTwoLifeTotal(int lifeTotal2) {
         TextView lifeView2 = (TextView) findViewById(R.id.life_player_two_text_view);
         lifeView2.setText(String.valueOf(lifeTotal2));
     }
 
-    public void displayPlayerOnePoisonTotal(int poisonTotal){
+    public void displayPlayerOnePoisonTotal(int poisonTotal) {
         TextView poisonView = (TextView) findViewById(R.id.player_one_poison_text_view);
         poisonView.setText(String.valueOf(poisonTotal));
     }
 
-    public void displayPlayerTwoPoisonTotal(int poisonTotal2){
+    public void displayPlayerTwoPoisonTotal(int poisonTotal2) {
         TextView poisonview2 = (TextView) findViewById(R.id.player_two_poison_text_view);
         poisonview2.setText(String.valueOf(poisonTotal2));
     }
 
-    public void displayPlayerOneEnergyTotal(int energyTotal){
+    public void displayPlayerOneEnergyTotal(int energyTotal) {
         TextView energyView = (TextView) findViewById(R.id.player_one_energy_text_view);
         energyView.setText(String.valueOf(energyTotal));
     }
 
-    public void displayPlayerTwoEnergyTotal(int energyTotal2){
+    public void displayPlayerTwoEnergyTotal(int energyTotal2) {
         TextView energyView2 = (TextView) findViewById(R.id.player_two_energy_text_view);
         energyView2.setText(String.valueOf(energyTotal2));
     }
@@ -139,15 +187,20 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    public void revealEnergyButton(View view){
+    public void revealEnergyButton(View view) {
         View energyCounterView = findViewById(R.id.player_one_energy_layout_view);
         View energyCounterView2 = findViewById(R.id.player_two_energy_layout_view);
-        if (energyCounterView.getVisibility() == View.VISIBLE){
+        if (energyCounterView.getVisibility() == View.VISIBLE) {
             energyCounterView.setVisibility(View.INVISIBLE);
             energyCounterView2.setVisibility(View.INVISIBLE);
         } else {
             energyCounterView.setVisibility(View.VISIBLE);
             energyCounterView2.setVisibility(View.VISIBLE);
         }
+    }
+
+    public void changeColors(View view) {
+        View v = findViewById(R.id.player_one_layout);
+        v.setBackgroundColor(getResources().getColor(R.color.magic_red));
     }
 }
